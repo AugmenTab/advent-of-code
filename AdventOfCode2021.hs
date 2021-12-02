79a067db3215858f66e52db92f8e1aededa00c5d
AugmenTab <tbaum92@live.com>
2021-12-01 18:44:55 -0700

    Adds solution for Day 1, Part 2

--- AdventOfCode2021.hs
+++ AdventOfCode2021.hs
@@ -5,8 +5,14 @@ day01 = do
   fileData <- readFile "data/2021/day01.txt"
   let depths    = map read $ lines fileData
       solution1 = depthIncreases depths
+      solution2 = depthWindows depths
 
   putStrLn $ "Part 1: " <> show solution1
+  putStrLn $ "Part 2: " <> show solution2
 
 depthIncreases :: [Int] -> Int
 depthIncreases xs = length $ filter (\(f,s) -> f < s) $ zip xs $ tail xs
+
+depthWindows :: [Int] -> Int
+depthWindows xs = depthIncreases windows
+  where windows = zipWith3 (\x y z -> x + y + z) xs (tail xs) (tail $ tail xs)
