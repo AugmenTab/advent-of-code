cddf52f7e1a4ea0244134d7b6f0ae1e230dcc419
AugmenTab <tbaum92@live.com>
2021-12-01 22:50:22 -0700

    Adds solution for Day 2, Part 2

--- AdventOfCode2021.hs
+++ AdventOfCode2021.hs
@@ -23,13 +23,26 @@ day02 :: IO ()
 day02 = do
   fileData <- readFile "data/2021/day02.txt"
   let moves = map (splitOn " ") $ lines fileData
+      solution1 = calculatePosition (0, 0) moves
+      solution2 = complexPosition (0, 0, 0) moves
 
-  putStrLn $ show $ calculatePosition (0, 0) moves
+  putStrLn $ "Part 1: " <> show solution1
+  putStrLn $ "Part 2: " <> show solution2
 
 calculatePosition :: (Int, Int) -> [[String]] -> Int
 calculatePosition (h, d) [] = h * d
 calculatePosition (h, d) ((dir:num:[]):xs)
-  | dir == "up"      = calculatePosition (h, d - (read num)) xs
-  | dir == "down"    = calculatePosition (h, d + (read num)) xs
-  | dir == "forward" = calculatePosition (h + (read num), d) xs
+  | dir == "up"      = calculatePosition (h, d - numInt) xs
+  | dir == "down"    = calculatePosition (h, d + numInt) xs
+  | dir == "forward" = calculatePosition (h + numInt, d) xs
   | otherwise        = calculatePosition (h, d) xs
+  where numInt = read num
+
+complexPosition :: (Int, Int, Int) -> [[String]] -> Int
+complexPosition (h, d, _) [] = h * d
+complexPosition (h, d, a) ((dir:num:[]):xs)
+  | dir == "up"      = complexPosition (h, d, a - numInt) xs
+  | dir == "down"    = complexPosition (h, d, a + numInt) xs
+  | dir == "forward" = complexPosition (h + numInt, d + (a * numInt), a) xs
+  | otherwise        = complexPosition (h, d, a) xs
+  where numInt = read num
