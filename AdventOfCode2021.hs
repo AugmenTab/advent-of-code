a7a8dc6e524ef3036d16c267638b989b2ebbcaed
AugmenTab <tbaum92@live.com>
2021-12-01 22:29:57 -0700

    Adds solution for Day 2, Part 1

--- AdventOfCode2021.hs
+++ AdventOfCode2021.hs
@@ -1,5 +1,7 @@
 module AdventOfCode2021 where
 
+import Data.List.Split (splitOn)
+
 day01 :: IO ()
 day01 = do
   fileData <- readFile "data/2021/day01.txt"
@@ -16,3 +18,18 @@ depthIncreases xs = length $ filter (\(f,s) -> f < s) $ zip xs $ tail xs
 depthWindows :: [Int] -> Int
 depthWindows xs = depthIncreases windows
   where windows = zipWith3 (\x y z -> x + y + z) xs (tail xs) (tail $ tail xs)
+
+day02 :: IO ()
+day02 = do
+  fileData <- readFile "data/2021/day02.txt"
+  let moves = map (splitOn " ") $ lines fileData
+
+  putStrLn $ show $ calculatePosition (0, 0) moves
+
+calculatePosition :: (Int, Int) -> [[String]] -> Int
+calculatePosition (h, d) [] = h * d
+calculatePosition (h, d) ((dir:num:[]):xs)
+  | dir == "up"      = calculatePosition (h, d - (read num)) xs
+  | dir == "down"    = calculatePosition (h, d + (read num)) xs
+  | dir == "forward" = calculatePosition (h + (read num), d) xs
+  | otherwise        = calculatePosition (h, d) xs
