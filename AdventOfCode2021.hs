ea92df740b65e6c7809c3d2a9a357d0985542636
AugmenTab <tbaum92@live.com>
2021-12-02 22:43:44 -0700

    Adds solution for Day 3, Part 1

--- AdventOfCode2021.hs
+++ AdventOfCode2021.hs
@@ -1,5 +1,6 @@
 module AdventOfCode2021 where
 
+import Data.List       (transpose)
 import Data.List.Split (splitOn)
 
 day01 :: IO ()
@@ -46,3 +47,26 @@ complexPosition (h, d, a) ((dir:num:[]):xs)
   | dir == "forward" = complexPosition (h + numInt, d + (a * numInt), a) xs
   | otherwise        = complexPosition (h, d, a) xs
   where numInt = read num
+
+day03 :: IO ()
+day03 = do
+  fileData <- readFile "data/2021/day03.txt"
+  let binaries  = lines fileData
+      solution1 = findPowerConsumption (transpose binaries)
+
+  putStrLn $ "Part 1: " <> show solution1
+
+findPowerConsumption :: [String] -> Int
+findPowerConsumption bs = (binToInt $ reverse gamma) * (binToInt $ reverse epsilon)
+  where mkPairs = foldr (\x (a, b) -> if x == '0'
+                                         then (a + 1, b)
+                                         else (a, b + 1))
+                        (0,0)
+        pairs   = map mkPairs bs
+        gamma   = map (\(x, y) -> if x > y then 0 else 1) pairs
+        epsilon = map (\(x, y) -> if x > y then 1 else 0) pairs
+
+binToInt :: [Int] -> Int
+binToInt bin = go bin 0
+  where go []     _ = 0
+        go (x:xs) n = (x * (2^n)) + (go xs (n + 1))
