67aac979b3dcb0babc6aaf73a966025b06be1dc5
AugmenTab <tbaum92@live.com>
2022-12-02 19:51:05 -0700

    2022 Day 01 Part 2

--- 2022/AdventOfCode2022.hs
+++ 2022/AdventOfCode2022.hs
@@ -7,8 +7,11 @@ import qualified Data.Text as T
 
 day01 :: IO ()
 day01 = do
+  fileData <- readFile "data/2022/day01.txt"
+
   let sumCalories = sum . fmap (read . T.unpack)
       getCalories = fmap (sumCalories . T.lines) . T.splitOn "\n\n" . T.pack
+      allCalories = reverse . L.sort $ getCalories fileData
 
-  fileData <- readFile "data/2022/day01.txt"
-  putStrLn . show . listToMaybe . reverse . L.sort $ getCalories fileData
+  putStrLn $ "Most calories: " <> show (listToMaybe allCalories)
+  putStrLn $ "Top 3 calories: " <> show (sum $ L.take 3 allCalories)
